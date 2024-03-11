494257258ac8a52bad498b74e552c36fecb337d9
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-03-11 16:59:58 -0700

    Add files via upload

---
+++ Options/bb_train_options.py
@@ -0,0 +1,27 @@
+import argparse
+
+class TrainOptions():
+
+    def initialize(self, parser):
+        self.parser = parser
+        parser.add_argument('--data_dir', type=str,  help='Root path to the data')
+        parser.add_argument('--pathologies',type=str, default='CNV,Drusen,DME,Normal',  help='Comma Spreaded List of Labels to predict for Pre-training')
+        parser.add_argument('--meta_csv', type=str,  default='/home/berkin/Kermany_meta_mock2.csv',help='Path to meta csv file')
+        parser.add_argument('--out_dir', type=str, default='./', help='Output directory to save the trained model')
+        parser.add_argument('--gpu_id', type=int ,default=0, help='GPU ID for training')
+        parser.add_argument('--b_size', type=int ,default=4, help='Batch size for training')
+        parser.add_argument('--n_cpu', type=int ,default=32, help='# of cpus to use for loading data')
+        return self.parser
+    
+    def parse(self):
+        opt = self.gather_options()
+        self.opt = opt
+        return self.opt
+    
+    def gather_options(self):
+        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser = self.initialize(parser)
+        # get the basic options
+        opt, _ = parser.parse_known_args()
+        self.parser = parser
+        return parser.parse_args()
