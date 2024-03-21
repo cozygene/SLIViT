cf98e5ac98bf6c7e78c43d48167ad38fdc64bea3
orenavram <orenavram@gmail.com>
2024-03-20 17:14:29 -0700

    code refactoring

--- Dsets/PDataset.py
+++ Dsets/PDataset.py
@@ -1,24 +1,21 @@
-import numpy as np
 import pandas as pd
 import torch
-from fastai.vision.augment import aug_transforms
 from torch.utils.data import Dataset
-from fastai.vision import *
 from utils.pretrain_auxiliaries import *
 
-
-default_transform = Compose(
+default_transform = tf.Compose(
     [
         tf.ToPILImage(),
         tf.Resize((224, 224)),
         pil_contrast_strech(),
-        ToTensor(),
+        tf.ToTensor(),
         gray2rgb
     ]
 )
 
+
 class PDataset(Dataset):
-    def __init__(self, metafile_path, annotations_path, data_dir,pathologies, transform=default_transform,
+    def __init__(self, metafile_path, annotations_path, data_dir, pathologies, transform=default_transform,
                  data_format='jpeg'):
         self.metadata = pd.read_csv(metafile_path)
         self.annotations = pd.read_csv(annotations_path)
@@ -31,19 +28,20 @@ class PDataset(Dataset):
         )[data_format]
         logger.info(f'Predicting {pathologies}')
         self.label_reader = get_labels
-        self.labels=[self.label_reader(self.samples[i], self.annotations, self.pathologies)[0][0] for i in range(len(self.samples))]
-        self.labels=torch.FloatTensor(self.labels)
-        self.data_dir=data_dir
+        self.labels = [self.label_reader(self.samples[i], self.annotations, self.pathologies)[0][0] for i in
+                       range(len(self.samples))]
+        self.labels = torch.FloatTensor(self.labels)
+        self.data_dir = data_dir
 
     def __len__(self):
         return len(self.samples)
-    
+
     def __getitem__(self, idx):
         sample = self.samples[idx[0]]
-        imgs = self.data_reader(sample,self.data_dir)
-        labels = self.label_reader(sample, self.annotations, self.pathologies) 
+        imgs = self.data_reader(sample, self.data_dir)
+        labels = self.label_reader(sample, self.annotations, self.pathologies)
         labels = torch.FloatTensor(labels)
-        t_imgs=self.t(imgs)
-        labels=labels.reshape(len(self.pathologies))
-   
+        t_imgs = self.t(imgs)
+        labels = labels.reshape(len(self.pathologies))
+
         return t_imgs, labels
--- bb_train.py
+++ bb_train.py
@@ -1,65 +1,76 @@
-import time
-from Options.bb_train_options import TrainOptions
-import numpy as np
-from transformers import ConvNextFeatureExtractor, ConvNextModel
-import pandas as pd
 import os
+import logging
+import wandb
+from Options.bb_train_options import args
+from utils.pretrain_auxiliaries import get_pathologies
+from fastai.callback.wandb import *
 
+wandb.init()
+
+if args.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    opt =  TrainOptions().parse()  
+    logger = logging.getLogger('main')
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(opt.gpu_id) 
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_id)
     from torch.utils.data import Subset
     from fastai.vision.all import *
     from fastai.data.transforms import TrainTestSplitter
     from Dsets.PDataset import PDataset
     from transformers import AutoModelForImageClassification
-    import torchvision.models as tmodels
-    dataset = PDataset(opt.meta_csv,
-                           opt.meta_csv,
-                           opt.data_dir,
-                        data_format='jpeg',
-                        pathologies= [p for p in opt.pathologies.split(',')] )
 
-    batch_size = opt.b_size
-    num_workers = opt.n_cpu
-    print(f'Num of cpus is {opt.n_cpu}')
-    print(f'Number of samples is {len(pd.read_csv(opt.meta_csv))}')
-    print(f'Batch size is {opt.b_size}')
+    pathologies = get_pathologies(args.meta_csv)
 
-    df=pd.read_csv(opt.meta_csv)
-    splts=[f.split('/')[1] for f in df['Path'].values]
-    
-    valid_dataset = Subset(dataset,np.argwhere(np.array(splts)=='test') )
-    train_dataset = Subset(dataset, np.argwhere(np.array(splts)=='train'))
+    dataset = PDataset(args.meta_csv,
+                       args.meta_csv,
+                       args.data_dir,
+                       data_format='jpeg',
+                       pathologies=pathologies
+                       )
+
+    batch_size = args.b_size
+    num_workers = args.n_cpu
+    logger.info(f'Num of cpus is {args.n_cpu}')
+    logger.info(f'Number of samples is {len(pd.read_csv(args.meta_csv))}')
+    logger.info(f'Batch size is {args.b_size}')
+
+    df = pd.read_csv(args.meta_csv)
+    splts = [f.split('/')[1] for f in df['Path'].values]
+
+    valid_dataset = Subset(dataset, np.argwhere(np.array(splts) == 'test'))
+    train_dataset = Subset(dataset, np.argwhere(np.array(splts) == 'train'))
     train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
 
-    print(f'# of train batches is {len(train_loader)}')
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True) 
-    print(f'# of validation batches is {len(valid_loader)}')
+    logger.info(f'# of train batches is {len(train_loader)}')
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
+    logger.info(f'# of validation batches is {len(valid_loader)}')
     dls = DataLoaders(train_loader, valid_loader)
     dls.c = 2
 
-    model2 = AutoModelForImageClassification.from_pretrained("facebook/convnext-base-224",return_dict=False,num_labels=len(opt.pathologies.split(',')),
-                                                            ignore_mismatched_sizes=True)
+    model2 = AutoModelForImageClassification.from_pretrained("facebook/convnext-base-224", return_dict=False,
+                                                             num_labels=len(pathologies),
+                                                             ignore_mismatched_sizes=True)
+
+
     class ConvNext(nn.Module):
         def __init__(self, model):
             super(ConvNext, self).__init__()
-            self.model=model
+            self.model = model
 
         def forward(self, x):
             x = self.model(x)[0]
             return x
-        
-    model=ConvNext(model2)
+
+
+    model = ConvNext(model2)
     model.to(device='cuda')
-    learner = Learner(dls, model, model_dir=opt.out_dir,
-                    loss_func=torch.nn.BCEWithLogitsLoss())
-    
+    learner = Learner(dls, model, model_dir=args.out_dir,
+                      loss_func=torch.nn.BCEWithLogitsLoss(), cbs=WandbCallback())
+
     learner.metrics = [RocAucMulti(average=None), APScoreMulti(average=None)]
 
-    learner.fit_one_cycle(n_epoch=opt.n_epochs, cbs=SaveModelCallback(fname='convnext_bb' ))
-
-
+    learner.fit_one_cycle(n_epoch=args.n_epochs, cbs=SaveModelCallback(fname='convnext_bb'))
