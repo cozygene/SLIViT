7efb572103c96cb03bfa5c5871f402f025c678cb
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-03-20 21:05:08 -0700

    Add files via upload

--- utils/pretrain_auxiliaries.py
+++ utils/pretrain_auxiliaries.py
@@ -1,34 +1,20 @@
-import io
-import json
+
 import logging
-import os
-from zipfile import ZipFile, BadZipFile
 import PIL
 import numpy as np
 from PIL import Image
 from skimage import exposure
 from torchvision import transforms as tf
-import torch
 from PIL import Image
-import torchvision
 import torchvision.transforms as transforms
-from torchvision.transforms import (
-    Compose,
-    Normalize,
-    RandomHorizontalFlip,
-    RandomResizedCrop,
-    ToTensor,
-)
 
-logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+
+
 
 gray2rgb = tf.Lambda(lambda x: x.expand(3, -1, -1))
 totensor = tf.Compose([
     tf.ToTensor(),
 ])
-
 
 def get_labels(sample, labels, pathologies):
     label=labels[(labels.Path ==sample)][pathologies].values
@@ -38,10 +24,7 @@ def get_samples(metadata, labels, pathologies):
     samples = []
     label_to_count = {p: str((labels[p]==1).sum())+' Positive Scans, ' +str((labels[p]==0).sum())+ ' Negative Scans'  for p in pathologies}
     for sample in metadata.Path.values:
-        sample_labels = get_labels(sample, labels, pathologies)
         samples.append(sample)
-        logger.debug(sample)
-    logger.info(f'Label counts is: {label_to_count}')
     print(f'Label counts is: {label_to_count}')
     return samples
 
@@ -58,7 +41,7 @@ transform_t = transforms.Compose([
         transforms.PILToTensor()
     ])
 
-def load_jpeg(vol_name,data_dir):
+def load_2dim(vol_name,data_dir):
     img=Image.open(data_dir+vol_name)
     img_tensor=transform_t(img)
     return img_tensor
