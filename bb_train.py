769de977c843a9355f75854226979b2092c78b92
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-03-20 21:06:10 -0700

    Add files via upload

--- bb_train.py
+++ bb_train.py
@@ -1,9 +1,8 @@
-import time
 from Options.bb_train_options import TrainOptions
 import numpy as np
-from transformers import ConvNextFeatureExtractor, ConvNextModel
 import pandas as pd
 import os
+
 
 
 if __name__ == '__main__':
@@ -11,41 +10,70 @@ if __name__ == '__main__':
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = str(opt.gpu_id) 
+    from medmnist import ChestMNIST
     from torch.utils.data import Subset
     from fastai.vision.all import *
-    from fastai.data.transforms import TrainTestSplitter
     from fastai.callback.wandb import *
-    from Dsets.PDataset import PDataset
+    from Dsets.KDataset import KDataset
+    from Dsets.XDataset import XDataset
     from fastai.callback.wandb import *
     from transformers import AutoModelForImageClassification
-    import torchvision.models as tmodels
-    dataset = PDataset(opt.meta_csv,
-                           opt.meta_csv,
-                           opt.data_dir,
-                        data_format='jpeg',
-                        pathologies= [p for p in opt.pathologies.split(',')] )
-
     batch_size = opt.b_size
     num_workers = opt.n_cpu
     print(f'Num of cpus is {opt.n_cpu}')
-    print(f'Number of samples is {len(pd.read_csv(opt.meta_csv))}')
-    print(f'Batch size is {opt.b_size}')
-
-    df=pd.read_csv(opt.meta_csv)
-    splts=[f.split('/')[1] for f in df['Path'].values]
     
-    valid_dataset = Subset(dataset,np.argwhere(np.array(splts)=='test') )
-    train_dataset = Subset(dataset, np.argwhere(np.array(splts)=='train'))
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
+    print(f'Batch size is {opt.b_size}')
+    if opt.dataset == 'kermany':
 
+        dataset = KDataset(opt.meta_csv,
+                            opt.meta_csv,
+                            opt.data_dir,
+                            data_format='jpeg',
+                            pathologies= [p for p in opt.pathologies.split(',')] )
+        df=pd.read_csv(opt.meta_csv)
+        splts=[p.split('/')[1] for p in df['Path'].values]
+        
+        valid_dataset = Subset(dataset,np.argwhere(np.array(splts)=='test') )
+        train_dataset = Subset(dataset, np.argwhere(np.array(splts)=='train'))
+        model2 = AutoModelForImageClassification.from_pretrained("facebook/convnext-base-224",return_dict=False,num_labels=len(opt.pathologies.split(',')),
+                                                            ignore_mismatched_sizes=True)
+    elif opt.dataset == 'chestmnist':
+        opt.meta_csv='NA'
+        opt.data_dir='NA'
+        opt.pathologies='NA'
+
+        test_dataset = ChestMNIST(split="test", download=True)
+        train_dataset = ChestMNIST(split="train", download=True)
+        valid_dataset= ChestMNIST(split="val", download=True)
+        train_dataset = XDataset(train_dataset)
+        valid_dataset = XDataset(valid_dataset)
+        model2 = AutoModelForImageClassification.from_pretrained("facebook/convnext-base-224",return_dict=False,num_labels=14,
+                                                            ignore_mismatched_sizes=True)
+    else:
+
+        dataset = CDataset(opt.meta_csv,
+                            opt.meta_csv,
+                            opt.data_dir,
+                            data_format='jpeg',
+                            pathologies= [p for p in opt.pathologies.split(',')] )
+        df=pd.read_csv(opt.meta_csv)
+        splts=[p.split('/')[1] for p in df['Path'].values]
+        
+        valid_dataset = Subset(dataset,np.argwhere(np.array(splts)=='test') )
+        train_dataset = Subset(dataset, np.argwhere(np.array(splts)=='train'))
+        model2 = AutoModelForImageClassification.from_pretrained("facebook/convnext-base-224",return_dict=False,num_labels=len(opt.pathologies.split(',')),
+                                                            ignore_mismatched_sizes=True)
+
+
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
     print(f'# of train batches is {len(train_loader)}')
     valid_loader = DataLoader(valid_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True) 
     print(f'# of validation batches is {len(valid_loader)}')
     dls = DataLoaders(train_loader, valid_loader)
     dls.c = 2
 
-    model2 = AutoModelForImageClassification.from_pretrained("facebook/convnext-base-224",return_dict=False,num_labels=len(opt.pathologies.split(',')),
-                                                            ignore_mismatched_sizes=True)
+    
     class ConvNext(nn.Module):
         def __init__(self, model):
             super(ConvNext, self).__init__()
@@ -62,6 +90,6 @@ if __name__ == '__main__':
     
     learner.metrics = [RocAucMulti(average=None), APScoreMulti(average=None)]
 
-    learner.fit_one_cycle(n_epoch=opt.n_epochs, cbs=SaveModelCallback(fname='convnext_bb' ))
+    learner.fit_one_cycle(n_epoch=opt.n_epochs, cbs=SaveModelCallback(fname='convnext_bb_'+opt.dataset))
 
 
