c05900be27df8a6904deefbc5e991a21158957b0
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-03-28 16:33:00 -0700

    Add files via upload

--- slivit_train.py
+++ slivit_train.py
@@ -1,120 +1,85 @@
+from Options.slivit_train_options import TrainOptions
+from utils.load_backbone import load_backbone
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-from torch.utils.data import Subset
-from fastai.vision.all import *
-from fastai.data.transforms import TrainTestSplitter
-from fastai.callback.wandb import *
-from model.slivit import load_backbone
-from model.slivit import SLIViT
-from fastai.callback.wandb import *
-from medmnist import NoduleMNIST3D
-from Dsets.NDataset import NDataset
-from torchvision import transforms as tf
-import numpy as np
-import torch
+
+if __name__ == '__main__':
+    opt =  TrainOptions().parse()  
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(opt.gpu_id) 
+    from fastai.vision.all import *
+    from fastai.callback.wandb import *
+    from Dsets.NDataset import NDataset
+    from fastai.callback.wandb import *
+    from torch.utils.data import Subset
+    from fastai.vision.all import *
+    from fastai.callback.wandb import *
+    from model.slivit import SLIViT
+    from fastai.callback.wandb import *
+    from medmnist import NoduleMNIST3D
+    from Dsets.UKBBDataset import UKBBDataset
+    import torch
+    
+    batch_size = opt.b_size
+    num_workers = opt.n_cpu
+    print(f'Num of cpus is {opt.n_cpu}')
+    warnings.filterwarnings('ignore')
+
+    if opt.dataset3d == 'nodulemnist':    
+        test_dataset = NoduleMNIST3D(split="test", download=True)
+        train_dataset = NoduleMNIST3D(split="train", download=True)
+        valid_dataset = NoduleMNIST3D(split="val", download=True)
+
+        test_dataset = NDataset(test_dataset,opt.nslc)
+        train_dataset = NDataset(train_dataset,opt.nslc)
+        valid_dataset = NDataset(valid_dataset,opt.nslc)
+    elif opt.dataset3d == 'ukbb':   
+        meta=pd.read_csv(opt.meta_csv)
+        train_indices =np.argwhere(meta['Split'].values=='train')
+        test_indices = np.argwhere(meta['Split'].values=='test')
+        valid_indices = np.argwhere(meta['Split'].values=='valid')
+        dataset = UKBBDataset(opt.meta_csv,
+                            opt.meta_csv,
+                            data_format='dcm',
+                            pathologies='PDFF')
+        train_dataset = Subset(dataset, train_indices)
+        valid_dataset = Subset(dataset, valid_indices)
+        test_dataset = Subset(dataset, test_indices)
+  
+    elif opt.dataset3d == 'custom3d':
+        test_dataset = NDataset(opt.test_dir,opt.nslc)
+        train_dataset = NDataset(opt.train_dir,opt.nslc)
+        valid_dataset = NDataset(opt.valid_dir,opt.nslc)
 
 
+    print()
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
+    print(f'# of train batches is {len(train_loader)}')
 
-warnings.filterwarnings('ignore')
-shuffles=np.arange(1)
-splitter = TrainTestSplitter(test_size=0.40)
-splitter2 = TrainTestSplitter(test_size=0.5)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
+    print(f'# of validation batches is {len(valid_loader)}')
 
-test_dataset = NoduleMNIST3D(split="test", download=True)
-train_dataset = NoduleMNIST3D(split="train", download=True)
-valid_dataset = NoduleMNIST3D(split="val", download=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
+    print(f'# of Test batches is {len(test_loader)}')
+    dls = DataLoaders(train_loader, valid_loader)
+    dls.c = 2
 
-test_dataset=NDataset(test_dataset,4)
-train_dataset = NDataset(train_dataset,4)
-valid_dataset = NDataset(valid_dataset,4)
+    backbone=load_backbone(opt.bbpath,opt.gpu_id)
+    model = SLIViT(backbone=backbone, image_size=(768, 64), patch_size=64, num_classes=1, dim=opt.dim, depth=opt.depth, heads=opt.heads,
+                    mlp_dim=opt.dim, channels=opt.nslc, dropout=opt.dropout, emb_dropout=opt.emb_dropout)
+    model.to(device='cuda')
 
-batch_size = 4
-num_workers = 64
+    if opt.task == 'classification':
+        loss_f=torch.nn.BCEWithLogitsLoss()
+    elif opt.task == 'regression':
+        loss_f=torch.nn.L1Loss()
 
+    learner = Learner(dls, model, model_dir=opt.out_dir ,
+                loss_func=loss_f )
+    
+    if opt.task == 'classification':
+        learner.metrics = [RocAucMulti(average=None), APScoreMulti(average=None)]
+    elif opt.task == 'regression':
+        learner.metrics =  [R2Score(),ExplainedVariance(),PearsonCorrCoef()]
 
-print()
+    learner.fit_one_cycle(n_epoch=opt.n_epochs, cbs=SaveModelCallback(fname='slivit'))
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
-print(f'# of train batches is {len(train_loader)}')
-
-valid_loader = DataLoader(valid_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
-print(f'# of validation batches is {len(valid_loader)}')
-
-test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
-print(f'# of Test batches is {len(test_loader)}')
-dls = DataLoaders(train_loader, valid_loader)
-dls.c = 2
-multi_gpu = False
-
-backbone = load_backbone('convnext')
-
-model = SLIViT(backbone=backbone, image_size=(768, 64), patch_size=64, num_classes=1, dim=64, depth=3, heads=10,
-                    mlp_dim=64, channels=4, dropout=0.2, emb_dropout=0.1)
-
-model.to(device='cuda')
-learner = Learner(dls, model, model_dir=f'/home/berkin/projects/hyper_rev2/' ,
-            loss_func=torch.nn.BCEWithLogitsLoss()
-            #loss_func= nn.CrossEntropyLoss(reduction='mean')
-            )
-
-fp16 = MixedPrecision()
-learner.metrics = [RocAucMulti(average=None), APScoreMulti(average=None)]
-print('Searching for learning rate...')
-learner.fit_one_cycle( lr=1e-5,n_epoch=10, cbs=SaveModelCallback(fname='slivit'))
-
-
-
-
-
-# t_model = learner.load('/home/berkin/projects/hyper_rev2/' +dis_n )
-# print('Required Task has Started')
-# valid_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
-# print(f'# of Test batches is {len(valid_loader)}')
-# xx1 = learner.get_preds(dl=valid_loader)
-# # np.savez('/home/berkin/projects/Amish_hybrid/Houston_slivit_kermany_new2/' + mod + '_' + label_t[
-# #  0] + '_nOslc_' + str(nOslices[kkk]) + '.npz', np.array(xx1))
-# act=nn.Sigmoid()
-# p2 = sklearn.metrics.roc_auc_score(xx1[1], act(xx1[0]))
-# per_test_l2.append(p2)
-# #np.savez('/scratch/dberkin/ORCID_new/Ricord_split_' + str(spl)+'/slivit_'+label_t[0] + '.npz', np.array(xx1))
-# #np.savez('/scratch/dberkin/ORCID_new/Ricord_split_' + str(spl) + '/slivit_roc' + '.npz', np.array(p2))
-# print('  Performance: ' + str(p2))
-
-# print(per_test_l2)
-# #print('Prevalance: '+ str(np.mean(xx1[1])))
-# scr=bt_trap(xx1)
-# np.savez('/home/berkin/projects/hyper_rev2/' +dis_n+'.npz' ,scr)
-# print(np.mean(scr))
-
-
-
-
-
-# if cntr ==0:
-#     dictty={'model':[dis_n]*100,'auc':scr.reshape(100),'Imaging Modality':['CT']*100}
-#     dfp=pd.DataFrame(dictty)
-# else:
-#     dictty={'model':[dis_n]*100,'auc':scr.reshape(100),'Imaging Modality':['CT']*100}
-#     #dfp2=pd.DataFrame(dictty)
-#     dfp=pd.concat([dfp,pd.DataFrame(dictty)])
-
-# cntr+=1
-
-
-# print()
-# plt.figure()
-# plt.ylim(0.5,1)
-# sns.boxplot(x='Imaging Modality', y='auc', data=dfp,hue='model',width=0.9,linewidth=0.5,showfliers = False,whis=[5,95]) ##dff whis=[5,95]
-# plt.legend(loc='lower right')
-# plt.show()
-# plt.savefig('slivit_figure.png',dpi=200)
-
-
-
-
-
-
-
-
-            
