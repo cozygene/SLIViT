7138ad6e400e65f0bd10488138da06008dfe217f
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-04-01 13:53:43 -0700

    Add files via upload

---
+++ Splits/noduleMNIST/noduleMNIST_train_ids.py
@@ -0,0 +1,3 @@
+from medmnist import NoduleMNIST3D
+# Load Train Data
+train_dataset = NoduleMNIST3D(split="train", download=True)
---
+++ Splits/noduleMNIST/noduleMNIST_val.py
@@ -0,0 +1,3 @@
+from medmnist import NoduleMNIST3D
+# Load Validation Data
+valid_dataset = NoduleMNIST3D(split="val", download=True)
