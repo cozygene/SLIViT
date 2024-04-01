fd6b8cea6c4d09c876e2480a2069a1a3a0b3ed02
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-04-01 13:53:00 -0700

    Add files via upload

---
+++ Splits/chestMNIST/chestMNIST_test_ids.py
@@ -0,0 +1,3 @@
+from medmnist import ChestMNIST
+# Load Test Data
+test_dataset = ChestMNIST(split="test", download=True)
---
+++ Splits/chestMNIST/chestMNIST_val_ids.py
@@ -0,0 +1,3 @@
+from medmnist import ChestMNIST
+# Load Validation Data
+val_dataset = ChestMNIST(split="val", download=True)
