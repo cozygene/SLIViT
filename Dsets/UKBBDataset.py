fe4df694966b1f206836ac15dc9b88505c327ef2
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-04-03 23:37:33 -0700

    Add files via upload

--- Dsets/UKBBDataset.py
+++ Dsets/UKBBDataset.py
@@ -10,32 +10,29 @@ from fastai.vision import *
 default_transform_gray = tf.Compose([
     tf.ToPILImage(),
     tf.Resize((256, 256)),
-    pil_contrast_strech(),
+    #pil_contrast_strech(),
     tf.ToTensor(),
     gray2rgb
 ])
 class UKBBDataset(Dataset):
-    def __init__(self, metafile_path, annotations_path, pathologies, transform=default_transform_gray,
-                 data_format='dcm'):
+    def __init__(self, metafile_path, annotations_path, pathologies, nslc,transform=default_transform_gra):
         self.metadata = pd.read_csv(metafile_path)
         self.annotations = pd.read_csv(annotations_path)
         self.pathologies = pathologies
         self.samples = get_samples(self.metadata, self.annotations, pathologies)
         self.t = default_transform_gray
-        self.data_reader = dict(
-            dcm=load_dcm
-        )[data_format]
-
+        self.data_reader = load_dcm
         self.label_reader = get_labels
         self.labels=[self.label_reader(self.samples[i], self.annotations, self.pathologies) for i in range(len(self.samples))]
         self.labels=torch.FloatTensor(self.labels)
+        self.nlsc=nslc
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
         sample = self.samples[idx[0]]
-        imgs = self.data_reader(sample)
+        imgs = self.data_reader(sample,self.nlsc)
         labels = self.label_reader(sample, self.annotations, self.pathologies)  
         labels = torch.FloatTensor(labels)
         t_imgs = torch.cat([self.t(im) for im in imgs], dim=-1)
