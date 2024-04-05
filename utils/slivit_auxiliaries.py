9d93cb3a806dd9d9e26a489a9896630567d0e360
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-04-05 16:45:05 -0700

    Add files via upload

--- utils/slivit_auxiliaries.py
+++ utils/slivit_auxiliaries.py
@@ -48,6 +48,6 @@ def load_dcm(path,nslc):
         for img_name in img_paths:
             img=dicom.dcmread(f'{path}/{img_name}')
             vol.append(totensor(img.pixel_array.astype(np.float64)))
-        idx_smpl=np.linspace(0, len(img_paths), nslc).astype(int)
+        idx_smpl=np.linspace(0, len(img_paths)-1, nslc).astype(int)
         vol = (np.array(vol)[idx_smpl]).tolist()
     return vol
