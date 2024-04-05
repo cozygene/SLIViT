ddb5808d64715a39c568531dd2ece9cf3c621f5c
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-04-05 14:16:36 -0700

    Add files via upload

--- Options/slivit_test_options.py
+++ Options/slivit_test_options.py
@@ -7,7 +7,8 @@ class TestOptions():
         parser = BaseOptions.initialize(self, parser) 
         parser.add_argument('--dataset3d',type=str, help='nodulemnist, ukbb, or custom3d')
         parser.add_argument('--checkpoint',type=str, default='./Pre-trained_Backbones/backbone.pth',  help='Path to fine tuned Slivit')
-        parser.add_argument('--depth', type=int, default=3, help='ViT depth')
+        parser.add_argument('--nObb_feat',type=int, default=4,  help='# of features backbone extracts')
+        parser.add_argument('--depth', type=int, default=5, help='ViT depth')
         parser.add_argument('--dim', type=int, default=256, help='ViT dim')
         parser.add_argument('--mlp_dim', type=int, default=512, help='fc dim')
         parser.add_argument('--nslc', type=int, default=28, help='# of slices to use for 3D Fine-tuning')
@@ -16,7 +17,6 @@ class TestOptions():
         parser.add_argument('--emb_dropout', type=float, default=0)
         parser.add_argument('--pathology',type=str,  help='Label to predict')
         parser.add_argument('--metric',type=str,  help='ROC-AUC,PR-AUC,R2')
-        
         return self.parser
     
     def parse(self):
--- Options/slivit_train_options.py
+++ Options/slivit_train_options.py
@@ -7,17 +7,15 @@ class TrainOptions():
         parser = BaseOptions.initialize(self, parser) 
         parser.add_argument('--dataset3d',type=str, help='nodulemnist, ukbb, or custom3d')
         parser.add_argument('--bbpath',type=str, default='./Pre-trained_Backbones/backbone.pth',  help='Path to pre-rained Convnext Backbone')
+        parser.add_argument('--nObb_feat',type=int, default=4,  help='# of features backbone extracts')
         parser.add_argument('--pathology',type=str,  help='Label to predict for 3D Fine-tuning')
         parser.add_argument('--task',type=str,  help='3D Fine-tuning task (classification or regression)')
-        parser.add_argument('--depth', type=int, default=3, help='ViT depth')
+        parser.add_argument('--depth', type=int, default=5, help='ViT depth')
         parser.add_argument('--dim', type=int, default=64, help='ViT depth')
         parser.add_argument('--nslc', type=int, default=28, help='# of slices to use for 3D Fine-tuning')
         parser.add_argument('--heads', type=int, default=10, help='# of heads for multihead attention')
         parser.add_argument('--dropout', type=float, default=0.2)
         parser.add_argument('--emb_dropout', type=float, default=0.1)
-        
-
-        
         return self.parser
     
     def parse(self):
