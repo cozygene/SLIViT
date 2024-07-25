df29171cfa712267d74f0e6ab2132159b77d18ba
orenavram <orenavram@gmail.com>
2024-07-25 15:09:54 -0700

    ViT code duplication removed (importing instead)

--- model/slivit.py
+++ model/slivit.py
@@ -1,118 +1,27 @@
-from vit_pytorch import ViT
-from torch import nn
-from einops import rearrange, repeat
+from vit_pytorch import vit
+from einops import repeat
 from einops.layers.torch import Rearrange
-from sklearn.metrics import precision_recall_curve
-from utils.slivit_auxiliaries import *
-import cv2
-from tqdm import tqdm
-import os
-from torch import nn
-from torchvision.transforms import (
-    Compose,
-    Normalize,
-    RandomHorizontalFlip,
-    RandomResizedCrop,
-    ToTensor,
-)
 from torchvision import transforms as tf
 import torch
-from transformers import AutoModelForImageClassification
 from torch import nn
 
 
 class ConvNext(nn.Module):
     def __init__(self, model):
         super(ConvNext, self).__init__()
-        self.model=model
+        self.model = model
 
     def forward(self, x):
         x = self.model(x)[0]
 
         return x
 
+
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
+
+
 gray2rgb = tf.Lambda(lambda x: x.expand(3, -1, -1))
-transform_new = tf.Compose(
-    [
-
-        tf.ToPILImage(),
-        tf.Resize((256, 256)),
-        pil_contrast_strech(),
-        #RandomResizedCrop((256,256)),
-        #RandomHorizontalFlip(),
-        ToTensor(),
-        #normalize,
-        gray2rgb
-])
-# classes
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
-
-
-class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout=0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            #nn.LeakyReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
-
-    def forward(self, x):
-        return self.net(x)
-
-
-class Attention(nn.Module):
-    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
-        super().__init__()
-        inner_dim = dim_head * heads
-        project_out = not (heads == 1 and dim_head == dim)
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-        self.attend = nn.Softmax(dim=-1)
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
-
-        self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, dim),
-            nn.Dropout(dropout)
-        ) if project_out else nn.Identity()
-
-    def forward(self, x):
-        qkv = self.to_qkv(x).chunk(3, dim=-1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
-        attn = self.attend(dots)
-        out = torch.matmul(attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
-
-
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
-            ]))
-
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-        return x
 
 
 class SLIViT(nn.Module):
@@ -120,7 +29,7 @@ class SLIViT(nn.Module):
                  channels=3, dim_head=64, dropout=0., emb_dropout=0.):
         super().__init__()
         self.backbone = backbone
-        self.channels=channels
+        self.channels = channels
         image_height, image_width = pair(image_size)
         _, patch_width = pair(patch_size)
         patch_height = 12 * patch_width
@@ -137,7 +46,7 @@ class SLIViT(nn.Module):
         self.pos_embedding = nn.Parameter(tmpp.reshape((1, tmpp.shape[0], tmpp.shape[1])))  # .cuda()
         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
         self.dropout = nn.Dropout(emb_dropout)
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+        self.transformer = vit.Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
         self.pool = pool
         self.to_latent = nn.Identity()
         self.mlp_head = nn.Sequential(
