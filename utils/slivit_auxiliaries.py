b6f6e3f99d48723f6a40064ea9ddc74fb05bc95d
orenavram <orenavram@gmail.com>
2024-09-19 17:14:58 -0700

    Fixing an edge case for evaluate.py

--- utils/slivit_auxiliaries.py
+++ utils/slivit_auxiliaries.py
@@ -86,8 +86,7 @@ default_transform_gray = tf.Compose([
 ])
 
 
-def get_split_indices(meta_csv, out_dir, split_ratio, pathology,
-                      split_col, pid_col):
+def get_split_indices(meta_csv, out_dir, split_ratio, pathology, split_col, pid_col):
     # try:
     #     df = pd.read_csv(split_file_path)
     #     train_idx = np.argwhere(df[split_col].str.contains('train', case=False))
@@ -104,33 +103,41 @@ def get_split_indices(meta_csv, out_dir, split_ratio, pathology,
         val_idx = np.argwhere(df[split_col].str.contains('val', case=False))
         test_idx = np.argwhere(df[split_col].str.contains('test', case=False))
     else:
-        logger.info(f'Creating split indices according to the provided split ratio: {split_ratio}')
-        # Draw indices for train, val and test
-        # First split
-        gss = GroupShuffleSplit(n_splits=1, train_size=split_ratio[0])
-        train_idx, temp_idx = next(gss.split(df, df[pathology],
-                                             df[pid_col]))  # split by patient
+        if split_ratio[-1] < 1:
+            logger.info(f'Creating split indices according to the provided split ratio: {split_ratio}')
+            # Draw indices for train, val and test
+            # First split
+            gss = GroupShuffleSplit(n_splits=1, train_size=split_ratio[0])
+            train_idx, temp_idx = next(gss.split(df, df[pathology],
+                                                 df[pid_col]))  # split by patient
 
-        if split_ratio[2] == 0:
-            # using external test set
-            val_idx = temp_idx
-            test_idx = temp_idx[[]]
+            if split_ratio[2] == 0:
+                # using external test set
+                val_idx = temp_idx
+                test_idx = temp_idx[[]]
+            else:
+                test_val_df = df.iloc[temp_idx]
+                # Second split
+                gss_temp = GroupShuffleSplit(n_splits=1, train_size=split_ratio[1] / sum(split_ratio[1:]))
+                val_idx, test_idx = next(gss_temp.split(test_val_df, test_val_df[pathology],
+                                                        test_val_df[pid_col]))  # split by patient
+
+                # Map temporary indices back to original indices
+                val_idx = temp_idx[val_idx]
+                test_idx = temp_idx[test_idx]
+
+            # Store split indices
+            df[split_col] = 'train'
+            df.loc[val_idx, split_col] = 'val'
+            df.loc[test_idx, split_col] = 'test'
+            df.to_csv(f'{out_dir}/{os.path.split(meta_csv)[-1]}', index=False)
         else:
-            test_val_df = df.iloc[temp_idx]
-            # Second split
-            gss_temp = GroupShuffleSplit(n_splits=1, train_size=split_ratio[1] / sum(split_ratio[1:]))
-            val_idx, test_idx = next(gss_temp.split(test_val_df, test_val_df[pathology],
-                                                    test_val_df[pid_col]))  # split by patient
-
-            # Map temporary indices back to original indices
-            val_idx = temp_idx[val_idx]
-            test_idx = temp_idx[test_idx]
-
-        # Store split indices
-        df[split_col] = 'train'
-        df.loc[val_idx, split_col] = 'val'
-        df.loc[test_idx, split_col] = 'test'
-        df.to_csv(f'{out_dir}/{os.path.split(meta_csv)[-1]}', index=False)
+            assert get_script_name() == 'evaluate', \
+                '--split_ratio was set inappropriately (empty train split is only allowed for evaluate.py)'
+            # evaluation_only
+            train_idx = np.array([])
+            val_idx = np.array([])
+            test_idx = np.arange(0, len(df))
 
     return train_idx, val_idx, test_idx
 
@@ -231,9 +238,9 @@ def get_volumetric_dataloaders(dataset_class, args, out_dir, mnist=None):
                 msg = f'Using external test set for final model evaluation from:\n{args.test_csv}'
                 test_df = pd.read_csv(args.test_csv)
                 test_subset = Subset(dataset_class(test_df, args.label3d, args.path_col, args.slices,
-                                                   args.sparsing_method), torch.arange(0, len(test_df)))
+                                                   args.sparsing_method), np.arange(0, len(test_df)))
 
-    if msg:
+    if msg and get_script_name() != 'evaluate':
         logger.info('\n\n' + '*' * 100 + f'\n{msg}\n' + '*' * 100 + '\n')
 
     logger.info(f'Num of cpus is {args.cpus}')
@@ -359,10 +366,9 @@ def create_learner(slivit, dls, out_dir, args):
     loss_f, metrics = get_loss_and_metrics(args.task)
     learner = Learner(dls, slivit, model_dir=out_dir, loss_func=loss_f, metrics=metrics,
                       cbs=[SaveModelCallback(fname=best_model_name),
-                           WandbCallback(),
-                           CSVLogger(),
                            EarlyStoppingCallback(monitor='valid_loss', min_delta=args.min_delta,
-                                                 patience=args.patience)])
+                                                 patience=args.patience),
+                           CSVLogger()] + [WandbCallback()] if get_script_name() != 'evaluate' else [])
     return learner, best_model_name
 
 
