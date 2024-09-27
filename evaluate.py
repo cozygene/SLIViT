a75989e725ff3d38576a2b19dd7dcf94134a77fe
orenavram <orenavram@gmail.com>
2024-09-26 21:04:36 -0700

    Adding an informative message when failed to load SLIViT

--- evaluate.py
+++ evaluate.py
@@ -7,9 +7,14 @@ if __name__ == '__main__':
 
     dls, test_loader = setup_dataloaders(args, out_dir)
 
-    slivit = SLIViT(backbone=load_backbone(args.fe_classes, args.fe_path),
-                    fi_dim=args.vit_dim, fi_depth=args.vit_depth, heads=args.heads, mlp_dim=args.mlp_dim,
-                    num_vol_frames=args.slices, dropout=args.dropout, emb_dropout=args.emb_dropout)
+    try:
+        slivit = SLIViT(backbone=load_backbone(args.fe_classes, args.fe_path),
+                        fi_dim=args.vit_dim, fi_depth=args.vit_depth, heads=args.heads, mlp_dim=args.mlp_dim,
+                        num_vol_frames=args.slices, dropout=args.dropout, emb_dropout=args.emb_dropout)
+    except RuntimeError as e:
+        logger.error(f"Could not load model:\n{e}\nPlease double-check that the pretrained feature extractor is "
+                     f"correctly set up and compatible with the model. This will ensure everything runs smoothly!\n")
+        sys.exit(1)
 
     learner, _ = create_learner(slivit, dls, out_dir, args)
 
