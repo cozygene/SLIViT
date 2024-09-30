480320fcc27031959a831054563d9d30066954da
orenavram <orenavram@gmail.com>
2024-09-29 19:52:18 -0700

    inheriting most of the logic from ViT

--- model/slivit.py
+++ model/slivit.py
@@ -49,59 +49,26 @@ class SLIViT(nn.Module):
         super().__init__()
         self.backbone = backbone  # TODO: call load_backbone here
         self.num_patches = num_of_patches
-        patch_dim = patch_height * patch_width  # 768 * 64
-        # assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        # Override random positional embedding initialization (by default)
+        if not rnd_pos_emb:
+            self.pos_embedding = nn.Parameter(
+                torch.arange(self.num_patches + 1).repeat(vit_dim, 1).t().unsqueeze(0).float()
+            )
+
+        # Override the patch embedding layer to handle feature-map patching (rather than image pathcing)
         self.to_patch_embedding = nn.Sequential(
             Rearrange('b c (h p1) (w p2) -> b (c h w) (p1 p2)', p1=patch_height, p2=patch_width),
-            nn.Linear(patch_dim, vit_dim),
-        )
-        # tmpp = torch.zeros((1, fi_width))
-        # positional_embeddings = torch.arange(self.num_patches) + 1
-        # for i in range(self.num_patches): tmpp = torch.concat([tmpp, torch.ones((1, fi_width)) * positional_embeddings[i]], axis=0)
-        # self.pos_embedding = nn.Parameter(tmpp.reshape((1, tmpp.shape[0], tmpp.shape[1])))  # .cuda()
-
-        if rnd_pos_emb:
-            # initialize positional embeddings randomly
-            # TODO: investigate if this is the best way to initialize positional embeddings
-            # TODO: it seems to work better for external crora
-            self.pos_embedding = nn.Parameter(torch.randn(1, self.num_patches + 1, vit_dim))
-        else:
-            # initialize positional embeddings with the slice number
-            self.pos_embedding = nn.Parameter(torch.arange(self.num_patches+1).repeat(vit_dim, 1).t().unsqueeze(0).float())  # require
-
-        self.cls_token = nn.Parameter(torch.randn(1, 1, vit_dim))
-        self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(vit_dim, vit_depth, heads, dim_head, mlp_dim, dropout)
-        self.pool = 'cls'
-        self.to_latent = nn.Identity()
-        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(patch_height * patch_width),
+            nn.Linear(patch_height * patch_width, vit_dim),
             nn.LayerNorm(vit_dim),
-            nn.Linear(vit_dim, num_classes)
-            #,nn.LayerNorm(num_classes)  #TODO: check if this is needed
         )
         self.act = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.backbone(x).last_hidden_state
-        # print(x.shape)
-        # x = x.last_hidden_state
-        # print(x.shape)
+        x = self.feature_extractor(x).last_hidden_state
         x = x.reshape((x.shape[0], self.num_patches, 768, 64))
-        # print(x.shape)
-        return ViT.forward(self, x)
-        # x = self.to_patch_embedding(x)
-        # b, n, _ = x.shape
-        # cls_tokens = repeat(self.cls_token, '() n d -> b n d', b=b)
-        # x = torch.cat((cls_tokens, x), dim=1)
-        # x += self.pos_embedding[:, :(n + 1)]
-        # x = self.dropout(x)
-        # x = self.transformer(x)
-        # # x = x.mean(dim=1) if self.pool == 'mean' else x[:, 0]
-        # x = x[:, 0]
-        # # x = self.to_latent(x)
-        # x = self.mlp_head(x)
-        # return x
+        return super().forward(x)
 
     # TODO: move load_backbone here
 
